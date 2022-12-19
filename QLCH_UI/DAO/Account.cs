86203cc67778f82eadc4fff072b29d7c95f24070
Dinhdu2602 <20520159@gm.uit.edu.vn>
2022-12-19 23:33:25 +0700

    DTO file 1st commit

---
+++ QLCH_UI/DAO/Account.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLCH_UI.DAO
+{
+    class Account
+    {
+        private static Account instance;
+
+        public static Account Instance
+        {
+            get { if (instance == null) instance = new Account(); return instance; }
+            private set => instance = value;
+        }
+
+        private Account() { }
+        public bool Login(string user, string password)
+        {
+
+            bool r = false;
+            string pass = Encrypt.Instance.Hash(password);
+            string query = "SELECT * FROM account WHERE username = N'" + user + "' AND mat_khau = N'" + pass + "' ";
+            DataTable rs = ConnectSQL.Instance.ExecuteQuery(query);
+            r = rs.Rows.Count > 0;
+            int OTP = QLCH_UI.DAO.Mail.Instance.getOTP();
+            if (r != true && OTP != 0)
+            {
+                if (OTP == Convert.ToInt32(password))
+                    r = true;
+            }
+            return r;
+        }
+        public int Register(string user, string email, string password)
+        {
+
+            string pass = Encrypt.Instance.Hash(password);
+            string query = "INSERT INTO account (username, email, mat_khau) VALUES ( @name , @email , @pass ); ";
+            int rs = ConnectSQL.Instance.ExecuteNonQuery(query, new object[] { user, email, pass });
+            return rs;
+        }
+        public string Check(string user, string password, string confirm, string email)
+        {
+            string result = "";
+
+
+            foreach (char i in user)
+            {
+                if ((i < '0' || i > '9') && (i < 'a' || i > 'z') && (i < 'A' || i > 'Z'))
+                {
+                    result = "Please choose username provided by the admin.";
+                    return result;
+                }
+            }
+            foreach (char i in password)
+            {
+                if ((i < '0' || i > '9') && (i < 'a' || i > 'z') && (i < 'A' || i > 'Z'))
+                {
+                    result = "Please choose another password.";
+                    return result;
+                }
+            }
+            if (user == "" || user == "Username")
+            {
+                result = "Please fill username.";
+                return result;
+            }
+            string query = "SELECT * FROM account WHERE username = @user ;";
+            string query_for_nv = "select * from nhan_vien where username = @user ;";
+            DataTable rs_ac = ConnectSQL.Instance.ExecuteQuery(query, new object[] { user });
+            DataTable rs_nv = ConnectSQL.Instance.ExecuteQuery(query_for_nv, new object[] { user });
+            if (rs_ac.Rows.Count > 0 || rs_nv.Rows.Count != 1)
+            {
+                result = "Username is already taken.Try another one.";
+                return result;
+            }
+            string queryy = "SELECT * FROM account WHERE email = @email ;";
+            DataTable rss = ConnectSQL.Instance.ExecuteQuery(queryy, new object[] { email });
+
+            if ((email.LastIndexOf("@gmail.com") + 10) != email.Length || email == "")
+            {
+                result = "It is not gmail. Please choose another email.";
+                return result;
+            }
+            if (rss.Rows.Count > 0)
+            {
+                result = "Email is already taken. Please choose another email.";
+                return result;
+            }
+            if (password != confirm || password == "")
+            {
+                result = "Confirm your password!";
+                return result;
+            }
+
+            return result;
+        }
+        public string Get_Loai_NV(string username)
+        {
+            string query = "select NHAN_VIEN.loainhanvien from ACCOUNT,NHAN_VIEN"
+                    + " where ACCOUNT.username = NHAN_VIEN.username and ACCOUNT.username = '" + username + "'";
+            DataTable rs = ConnectSQL.Instance.ExecuteQuery(query);
+            try
+            {
+                return rs.Rows[0]["loainhanvien"].ToString();
+            }
+            catch
+            {
+                return "admin";
+            }
+        }
+        public string Get_Ma_NV(string username)
+        {
+            string query = "select NHAN_VIEN.manv from ACCOUNT,NHAN_VIEN"
+                    + " where ACCOUNT.username = NHAN_VIEN.username and ACCOUNT.username = '" + username + "'";
+            DataTable rs = ConnectSQL.Instance.ExecuteQuery(query);
+            try
+            {
+                return rs.Rows[0]["manv"].ToString();
+            }
+            catch
+            {
+                return "NV01";
+            }
+        }
+        public bool Change_PW(string usermane, string pw)
+        {
+            string query = "update account set mat_khau = '" + pw + "'" +
+                           " where username = '" + usermane + "' ";
+
+            try
+            {
+                DataTable rs = ConnectSQL.Instance.ExecuteQuery(query);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public string Get_Username(string manv)
+        {
+            string query = "select ACCOUNT.username from ACCOUNT,NHAN_VIEN"
+                    + " where ACCOUNT.username = NHAN_VIEN.username and  NHAN_VIEN.manv = '" + manv + "'";
+            DataTable rs = ConnectSQL.Instance.ExecuteQuery(query);
+            try
+            {
+                return rs.Rows[0]["username"].ToString();
+            }
+            catch
+            {
+                return "huy";
+            }
+        }
+        public string Get_Ten(string manv)
+        {
+            string query = "select NHAN_VIEN.ten_nv from NHAN_VIEN"
+                    + " where NHAN_VIEN.manv = '" + manv + "'";
+            DataTable rs = ConnectSQL.Instance.ExecuteQuery(query);
+            try
+            {
+                return rs.Rows[0]["ten_nv"].ToString();
+            }
+            catch
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ QLCH_UI/DAO/ConnectSQL.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLCH_UI.DAO
+{
+    public class ConnectSQL
+    {
+        private static ConnectSQL instance;
+        private ConnectSQL() { }
+        public static ConnectSQL Instance
+        {
+            get { if (instance == null) instance = new ConnectSQL(); return instance; }
+            private set => instance = value;
+        }
+        private string connectionSTR;
+        public bool TestConnString()
+        {
+            //connectionSTR = @"Data Source=LAPTOP-0096GMC1;Initial Catalog=QL_CHDT;User ID=sa; PassWord=buiminhhuy;";
+            connectionSTR = ConfigurationManager.ConnectionStrings["Con"].ConnectionString;
+
+            //MessageBox.Show(connectionSTR);
+            bool isValidConnectionString = true;
+            try
+            {
+                var con = new SqlConnectionStringBuilder(connectionSTR);
+            }
+            catch (Exception)
+            {
+                // can be KeyNotFoundException, FormatException, ArgumentException
+                //MessageBox.Show(connectionSTR);
+                isValidConnectionString = false;
+            }
+            return isValidConnectionString;
+        }
+        public DataTable ExecuteQuery(string query, object[] paramater = null)
+        {
+            connectionSTR = ConfigurationManager.ConnectionStrings["Con"].ConnectionString;
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                if (paramater != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, paramater[i]);
+                            i++;
+                        }
+                    }
+                }
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+        public int ExecuteNonQuery(string query, object[] paramater = null)
+        {
+            connectionSTR = ConfigurationManager.ConnectionStrings["Con"].ConnectionString;
+            int data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                if (paramater != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, paramater[i]);
+                            i++;
+                        }
+                    }
+                }
+                data = command.ExecuteNonQuery();
+                connection.Close();
+            }
+            return data;
+        }
+
+        public object ExecuteScalar(string query, object[] paramater = null)
+        {
+            connectionSTR = ConfigurationManager.ConnectionStrings["Con"].ConnectionString;
+            object data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                if (paramater != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, paramater[i]);
+                            i++;
+                        }
+                    }
+                }
+                data = command.ExecuteScalar();
+                connection.Close();
+            }
+            return data;
+        }
+        public ReportDataSource Datasoucre(string mahd)
+        {
+            // this.reportViewer2.RefreshReport();
+            // ReportDataSource datasource = ConnectSQL.Instance.Datasoucre(ma);
+
+            string query = "select  HOA_DON.mahd, HOA_DOn.manv, dien_thoai, HOA_DON.ngay_ban , SAN_PHAM.gia_ban, HOA_DON.giam_gia, ten_sp, CTHD.so_luong,HOA_DON.tri_gia  from (HOA_DON inner join"
+                           + " (CTHD inner join SAN_PHAM on CTHD.masp = SAN_PHAM.masp) on HOA_DON.mahd = CTHD.mahd) inner join KHACH_HANG on KHACH_HANG.makh = HOA_DON.makh where HOA_DON.mahd = '" + mahd + "'";
+            SqlConnection con = new SqlConnection(connectionSTR);
+
+            con.Open();
+            //   SqlCommand Command = new SqlCommand(query, con);
+            DataSet1 ds = new DataSet1();
+            SqlDataAdapter da = new SqlDataAdapter(query, con);
+            da.Fill(ds, "Receipt");
+            con.Close();
+
+            ReportDataSource datasource = new ReportDataSource("DataSet_Report", ds.Tables[0]);
+            return datasource;
+        }
+    }
+}
+
