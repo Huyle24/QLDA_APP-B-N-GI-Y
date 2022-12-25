97754ec8afca0e12d13c47efc157971532358f6b
KaLord <Khoa4112002@gmail.com>
2022-12-25 12:51:47 +0700

    End of BUS

---
+++ QLCH_UI/BUS/CustomersBUS.cs
@@ -0,0 +1,95 @@
+﻿using QLCH_UI.DAO;
+using QLCH_UI.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLCH_UI.BUS
+{
+    public class CustomersBUS
+    {
+        private static CustomersBUS instance;
+
+        public static CustomersBUS Instance
+        {
+            get
+            {
+                if (instance == null) instance = new CustomersBUS();
+                return CustomersBUS.instance;
+            }
+            private set
+            {
+                CustomersBUS.instance = value;
+            }
+        }
+        private CustomersBUS() { }
+
+        // kiem tra ma 
+        public bool makh(string value)
+        {
+            int l = value.Length;
+            if (l == 0) return (false);
+            int k = 1;
+            if (l > 9) return (false);
+            for (int i = 0; i < l; i++)
+            {
+                if ((value[i] >= 65 && value[i] <= 90) || (value[i] >= 97 && value[i] <= 122) || (value[i] >= 48 && value[i] <= 57)) k = 1;
+                else return (false);
+            }
+            return (true);
+        }
+        // kiem tra SDT
+        public bool SDT(string value)
+        {
+            DataTable a = CustomersDAO.Instance.Customerslist();
+            for (int i=0;i<a.Rows.Count;i++)
+            {
+                if (a.Rows[i]["dien_thoai"].ToString() == value) return (false);
+            }    
+            int l = value.Length;
+            for (int i = 0; i < l; i++) if (value[i] < 48 || value[i] > 57) return (false);
+            if (l > 12 || l == 0) return (false);
+            return (true);
+        }
+        // Kiem tra gioi tinh
+        public bool gioi_tinh(bool a, bool b)
+        {
+            if (a == false && b == false) return (false);
+            return (true);
+        }
+        // kiem tra ten 
+        public bool ten_kh(string value)
+        {
+            int l = value.Length;
+            if (l > 49 || l == 0) return false;
+            return (true);
+        }
+        // kiem tra dia chi
+        public bool dia_chi(string value)
+        {
+            int l = value.Length;
+            if (l > 99 || l == 0) return false;
+            return (true);
+        }
+        public DataTable customerlist()
+        {
+            return CustomersDAO.Instance.Customerslist();
+        }
+        public bool insert_customer(CustomersDTO a)
+        {
+            return (CustomersDAO.Instance.Insert(a));
+        }
+        public bool edit_customer(CustomersDTO a)
+        {
+            return (CustomersDAO.Instance.Edit(a));
+        }
+        public bool delete_customer(string ID)
+        {
+            return (CustomersDAO.Instance.Delete(ID));
+        }
+    }
+}
---
+++ QLCH_UI/BUS/ProductBUS.cs
@@ -0,0 +1,142 @@
+﻿using QLCH_UI.DAO;
+using QLCH_UI.DTO;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace QLCH_UI.BUS
+{
+    public class ProductBUS
+    {
+        private static ProductBUS instance;
+
+        public static ProductBUS Instance
+        {
+            get
+            {
+                if (instance == null) instance = new ProductBUS();
+                return ProductBUS.instance;
+            }
+            private set
+            {
+                ProductBUS.instance = value;
+            }
+        }
+        private ProductBUS() { }
+
+        // kiem tra ma 
+        public bool masp(string value)
+        {
+            int l = value.Length;
+            if (l == 0) return (false);
+            int k = 1;
+            if (l > 9) return (false);
+            for (int i = 0; i < l; i++)
+            {
+                if ((value[i] >= 65 && value[i] <= 90) || (value[i] >= 97 && value[i] <= 122) || (value[i] >= 48 && value[i] <= 57)) k = 1;
+                else return (false);
+            }
+            return (true);
+        }
+        // kiem tra ten 
+        public bool ten_sp(string value)
+        {
+            int l = value.Length;
+            if (l > 49 || l == 0) return false;
+            return (true);
+        }
+        //kiem tra loai_sp
+        public bool loai_sp(string value)
+        {
+            int l = value.Length;
+            if (l == 0) return (false);
+            return (true);
+        }
+        //gia nhap
+        public bool gia_nhap(string value)
+        {
+            int l = value.Length;
+            if (l == 0) return (false);
+            if (value[0] == '.') return (false);
+            int d = 0;
+            for (int i = 0; i < l; i++)
+                if (value[i] == '.')
+                {
+                    d++;
+                    if (d == 2) return (false);
+                }
+                else
+                {
+                    if ((value[i] < 48 || value[i] > 57) && (value[i] != ',')) return (false);
+                }
+            return (true);
+        }
+        // gia ban
+        public bool gia_ban(string value)
+        {
+            int l = value.Length;
+            if (l == 0) return (false);
+            if (value[0] == '.') return (false);
+            int d = 0;
+            for (int i = 0; i < l; i++)
+                if (value[i] == '.')
+                {
+                    d++;
+                    if (d == 2) return (false);
+                }
+                else
+                {
+                    if ((value[i] < 48 || value[i] > 57) && (value[i] != ',')) return (false);
+                }
+            return (true);
+        }
+        // kiem tra so luong
+        public bool so_luong(string value)
+        {
+            int l = value.Length;
+            if (l == 0) return (false);
+            for (int i = 0; i < l; i++)
+                if (value[i] < 48 || value[i] > 57) return (false);
+            return (true);
+        }
+        public Image ByteToImg (string byteString)
+        {
+            byte[] imgBytes = Convert.FromBase64String(byteString);
+            MemoryStream ms = new MemoryStream(imgBytes, 0, imgBytes.Length);
+            ms.Write(imgBytes, 0, imgBytes.Length);
+            Image image = Image.FromStream(ms, true);
+            return image;
+        }
+
+        public byte[] ImageToByteArray(string a)
+        {
+            FileStream fs;
+            fs = new FileStream(a, FileMode.Open, FileAccess.Read);
+            byte[] picbyte = new byte[fs.Length];
+            fs.Read(picbyte, 0, System.Convert.ToInt32(fs.Length));
+            fs.Close();
+            return picbyte;
+        }
+
+        public bool insert_product(ProductDTO a)
+        {
+            return (ProductDAO.Instance.insert(a));
+        }
+        public bool delete_product(string ID)
+        {
+            return (ProductDAO.Instance.Delete(ID));
+        }
+        public bool edit_product(ProductDTO a)
+        {
+            return (ProductDAO.Instance.Edit(a));
+        }
+    }
+}
